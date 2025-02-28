4a1e7820fc68f3c73e06fd4d3bbb788b8712b5b7
borja <borjaahedo@gmail.com>
2025-02-28 11:18:53 +0100

    push with the merge contact mail

--- app/Models/MailingList.php
+++ app/Models/MailingList.php
@@ -6,6 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class MailingList extends Model
 {
+    // Protect attributes from mass-assignment unless explicitly allowed
+    protected $fillable = [
+        'name',
+        'description',
+        'creation_date',
+        'last_updated_date',
+        'owner_id', // Foreign key
+        'status',
+        'type',
+        'tags'
+    ];
     public function contacts()
     {
         return $this->belongsToMany(Contact::class);
---
+++ database/migrations/2025_02_28_082833_create_mail_list.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+  public function up(): void{Schema::create('mailing_lists', function (Blueprint $table) {// Primary key$table->id();
+
+            // Attributes
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->date('creation_date');
+            $table->date('last_updated_date');
+            $table->foreignId('owner_id')->constrained('users')->onDelete('cascade'); // Foreign key to Users table
+            $table->enum('status', ['active', 'draft', 'archived']);
+            $table->enum('type', ['newsletter', 'promotions', 'updates']); // You can adjust the types as necessary
+            $table->string('tags')->nullable(); // Can store categories/tags as a string or as JSON
+
+            // Timestamps for created_at and updated_at
+            $table->timestamps();
+        });
+    }
+  public function down(): void{Schema::dropIfExists('mailing_lists');}
+};
