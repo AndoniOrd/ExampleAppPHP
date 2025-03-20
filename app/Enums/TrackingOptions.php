abb33ab966367f07c4d7151f9d3772c53703b397
borja <borjaahedo@gmail.com>
2025-03-20 11:33:19 +0100

    now api planning working 4 sure

--- app/Enums/TrackingOptions.php
+++ app/Enums/TrackingOptions.php
@@ -5,6 +5,7 @@ namespace App\Enums;
 enum TrackingOptions: string
 {
     case OPEN_CLICK = 'open_click';
+    case CLICKS = 'clicks'; 
     case BOUNCES = 'bounces';
     case ALL = 'all';
     case NONE = 'none';
