c57288b9a5bcdb761ab7282821f2ed053ecfe08d
Damian Koziołek <damikozi223@gmail.com>
2025-05-29 13:06:01 +0200

    add to kernel auto sending scheduler

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,9 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+\Illuminate\Support\Facades\Schedule::command('emails:send')
+    ->everyMinute()
+    ->withoutOverlapping()
+    ->appendOutputTo(storage_path('logs/email-campaign.log'));
